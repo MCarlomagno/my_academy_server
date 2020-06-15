39f0c95097241c4a1e7ca8140dae26a14a4e0b6b
mcarlomagno <marcoscarlomagno2@gmail.com>
2020-06-15 01:36:15 -0300

    config: data base connection

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -86,6 +89,45 @@ func main() {
 	///
 	router.GET("/courses", func(c *gin.Context) {
 		c.String(http.StatusOK, "Get a courses!")
+
+		//we open the database
+		db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+
+		//creating the statement
+		sqlStatement := `SELECT id, title FROM courses WHERE id=$1;`
+
+		// creating variables to take result
+		var title string
+		var id int
+
+		//Querying
+		row := db.QueryRow(sqlStatement, 1)
+
+		// scaning result
+		switch err := row.Scan(&id, &title); err {
+		case sql.ErrNoRows:
+			fmt.Println("No rows were returned!")
+		case nil:
+			fmt.Println(id, title)
+		default:
+			panic(err)
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer db.Close()
+
+		var msg struct {
+			Ttile string
+			Id    int
+		}
+
+		msg.Ttile = title
+		msg.Id = id
+
+		// response
+		c.JSON(http.StatusOK, msg)
 	})
 	router.POST("/courses", func(c *gin.Context) {
 		c.String(http.StatusOK, "post a courses!")
