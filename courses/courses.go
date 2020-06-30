8edc353a0a03af0c222dbad51129c5150c10aaa1
mcarlomagno <marcoscarlomagno2@gmail.com>
2020-06-30 08:12:38 -0300

    add: user created courses service

--- courses/courses.go
+++ courses/courses.go
@@ -62,6 +62,49 @@ func GetCoursesRoot(c *gin.Context) {
 
 }
 
+// GetUserCreatedCourses function
+func GetUserCreatedCourses(c *gin.Context) {
+	//we open the database
+	db, errA := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if errA != nil {
+		fmt.Println("error opening db")
+	}
+	//closing connection
+	defer db.Close()
+
+	//creating the statement
+	sqlStatement := `SELECT id, id_user, title, description FROM courses WHERE id_user=$1;`
+
+	//Querying
+	rows, errB := db.Query(sqlStatement, c.Param("ownerUserId"))
+	if errB != nil {
+		// handle this error better than this
+		fmt.Println("error opening rows")
+	}
+	defer rows.Close()
+
+	courses := make([]*Course, 0)
+
+	for rows.Next() {
+		var id int
+		var title string
+		var ownerUserID int
+		var description string
+		course := new(Course)
+		errC := rows.Scan(&id, &ownerUserID, &title, &description)
+		if errC != nil {
+			fmt.Println(errC)
+		}
+		course.ID = id
+		course.OwnerUserID = ownerUserID
+		course.Title = title
+		course.Description = description
+		courses = append(courses, course)
+	}
+	// response
+	c.JSON(http.StatusOK, courses)
+}
+
 // PostCoursesRoot function
 func PostCoursesRoot(c *gin.Context) {
 	//we open the database
